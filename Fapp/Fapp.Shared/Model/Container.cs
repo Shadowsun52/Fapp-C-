065dfbfb63e1a48f80c19fc2d582f19f46e4035f
Shadowsun52 <jenicot.alexandre@gmail.com>
2014-11-05 16:46:03 +0100

    creation des objets : -user -container -item

---
+++ Fapp/Fapp.Shared/Model/Container.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fapp.Model
+{
+    public class Container
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime lastSync { get; set; }
+        //le type est en int pour diminuer la taille des données
+        //envoyées et parce qu'il y a peut de type (frigo et congel)
+        public int type { get; set; }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
