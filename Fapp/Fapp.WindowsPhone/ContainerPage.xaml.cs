3d19a70effa3577c26d01e30bbfb0c859b262a17
zamerine <ptitlelex@hotmail.com>
2014-11-18 17:59:00 +0100

    un peu de view

--- Fapp/Fapp.WindowsPhone/ContainerPage.xaml.cs
+++ Fapp/Fapp.WindowsPhone/ContainerPage.xaml.cs
@@ -7,7 +7,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
+using windows.ui.xaml.controls.primitive;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
